90fba53d5e78da322709e5ef3b07117271a1cdd5
d35h46 <d35h46@daum.net>
2024-09-21 03:04:44 +0900

    fix: callbak login

--- web/daily/src/components/Login.jsx
+++ web/daily/src/components/Login.jsx
@@ -1,15 +1,10 @@
 function Login(){
 
-    const LoginKakao = () =>{
-
-        fetch('/auth/kakao/callback')
-    }
-
     return(
         <div>
             <div id='login-bg' className="flex flex-col w-full h-screen justify-center items-center bg-indigo-300">
                 <img src="/menu_home.svg" className="w-32 pb-10"/>
-                <a href="https://daeco.vercel.app/auth/kakao/callback">
+                <a href="/auth/kakao/callback">
                     <img src="/kakao_login_large_narrow.png" className="w-52"/>
                 </a>
             </div>
