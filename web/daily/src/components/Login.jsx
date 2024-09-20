12fa61fa6690b87af4aa5a1ad8dcd762568c45b2
d35h46 <d35h46@daum.net>
2024-09-21 03:03:34 +0900

    feat:callback login

--- web/daily/src/components/Login.jsx
+++ web/daily/src/components/Login.jsx
@@ -1,16 +1,17 @@
 function Login(){
 
     const LoginKakao = () =>{
-        alert('hi')
+
+        fetch('/auth/kakao/callback')
     }
 
     return(
         <div>
             <div id='login-bg' className="flex flex-col w-full h-screen justify-center items-center bg-indigo-300">
                 <img src="/menu_home.svg" className="w-32 pb-10"/>
-                <button onClick={LoginKakao}>
+                <a href="https://daeco.vercel.app/auth/kakao/callback">
                     <img src="/kakao_login_large_narrow.png" className="w-52"/>
-                </button>
+                </a>
             </div>
         </div>
     )
