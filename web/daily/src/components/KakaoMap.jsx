fa16deaf4c42dbb325ca71fe78730c71a76ae907
d35h46 <d35h46@daum.net>
2024-09-21 01:54:58 +0900

    feat: add KakaoMap

---
+++ web/daily/src/components/KakaoMap.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react"
+const { kakao } = window
+
+function kakaoMap(){
+    var lat = 37.365264512305174
+    var lng = 127.10676860117488
+    
+    useEffect(() => {
+        const container = document.getElementById("feed-map");
+        const options = {
+          center: new kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3,
+        };
+        const map = new kakao.maps.Map(container, options);
+      }, [])
+
+    return(
+        <div id='feed-map' className="w-full h-full z-0 mx-auto mt-5 rounded-xl"/>
+
+    )
+}
+
+export default kakaoMap
