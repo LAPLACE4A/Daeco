60067077914b3a80c5d055819e7dce227856209c
kimhee0129 <kimhee0129@gmail.com>
2024-09-22 15:13:41 +0900

    [chore] 코드 정리

--- application/server/recommend/views.py
+++ application/server/recommend/views.py
@@ -3,13 +3,33 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+import recommend_DC as r
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 # Create your views here.
 @api_view(['POST'])
 
 def recommend(request):
     data = request.data
 
-    response_data = {"age": data["age"]*10,
+    try:
+        result = r.recommend(data["gender"],
+                             data["age"],
+                             data["mbti"],
+                             data["weather"],
+                             data["amount"])
+    except:
+        error_response = {
+            'message': '잘못된 json 수신!',
+            'status': 'error'
+        }
+        return Response(error_response, status=status.HTTP_400_BAD_REQUEST)
+    
+    result = list(map(list, result))
+
+    response_data = {"recommend": result,
                      "status": "success"}
     
     return Response(response_data, status=status.HTTP_200_OK)
